0a679d664bb2f358d31dffef9c6bbe3b62c9422e
Augustine.Oseafiana <augustine.oseafiana@gmail.com>
2023-04-17 19:36:30 -0600

    first commit

---
+++ src/main/java/com/comit/users/Comit1UsersSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.comit.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comit1UsersSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comit1UsersSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comit/users/Comit1UsersSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comit.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comit1UsersSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
